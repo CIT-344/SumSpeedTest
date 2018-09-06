0fc3b50876f476df16a5b99114b7ae6d3f62d789
Bailey Miller <bmiller@susquehannasoftware.com>
2018-09-06 12:50:22 -0400

    dotnet new console -- Created the base project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SumSpeedTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
